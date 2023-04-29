1c796c27ba5b4f76bd24b032d55ea2d1c9d029cc
Reinaldo Mendoza <rmendoza83@gmail.com>
2023-04-29 13:41:12 -0500

    adding default libraries to use on frontend, fixed ts configuration and build errors for vite

--- TodoCrud.Client/src/main.ts
+++ TodoCrud.Client/src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+import './sass/styles.scss'
 
 const app = createApp(App)
 
--- TodoCrud.Client/vite.config.ts
+++ TodoCrud.Client/vite.config.ts
@@ -2,14 +2,29 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as sass from 'sass'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      sass: {
+        implementation: sass
+      }
+    }
+  },
   plugins: [vue()],
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+    alias: [
+      {
+        find: '@',
+        replacement: fileURLToPath(new URL('./src', import.meta.url))
+      },
+      {
+        find: /~(.+)/,
+        replacement: `${process.cwd()}/node_modules/$1`,
+      }
+    ]
   },
   server: {
     port: 3000,
